ec314ed250437791044c7c368056efd744466b89
sheneshbrayan128-lab <sheneshbrayan128@gmail.com>
2026-07-22 22:46:33 +0530

    Update fallback-data.js

--- fallback-data.js
+++ fallback-data.js
@@ -14,7 +14,7 @@ export const FALLBACK = {
     {
       "name": "Rebar Mesh",
       "subcategory": "Rebar Mesh (දැල්)",
-      "image": "https://cdn.pixabay.com/photo/2018/10/09/08/10/construction-3734383_1280.jpg",
+      "image": "<blockquote class="imgur-embed-pub" lang="en" data-id="a/SJGdBUA" data-context="false" ><a href="//imgur.com/a/SJGdBUA"></a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>",
       "variants": [
         "2¼\" x 2¼\"",
         "2½\" x 2½\""
