5447803eb9c62568afc7d27e772bc0f2aa413523
sheneshbrayan128-lab <sheneshbrayan128@gmail.com>
2026-07-22 10:35:48 +0530

    Update fallback-data.js

--- fallback-data.js
+++ fallback-data.js
@@ -14,7 +14,7 @@ export const FALLBACK = {
     {
       "name": "Rebar Mesh",
       "subcategory": "Rebar Mesh (දැල්)",
-      "image": "",
+      "image": "https://cdn.pixabay.com/photo/2018/10/09/08/10/construction-3734383_1280.jpg",
       "variants": [
         "2¼\" x 2¼\"",
         "2½\" x 2½\""
